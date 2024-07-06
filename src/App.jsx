ccbc00603f783d2138a2b88c7ae37333f09b0158
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2024-07-06 15:26:23 -0400

    transcribing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 import Header from "./components/Header"
 import HomePage from "./components/HomePage"
 import FileDisplay from "./components/FileDisplay"
+import Information from "./components/Information"
+import Transcribing from "./components/Transcribing"
 
 function App() {
   const [file, setFile] = useState(null) // audio file
   const [audioStream, setAudioStream] = useState(null) // live recording 
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   const isAudioAvailable = file || audioStream
 
@@ -19,21 +23,28 @@ function App() {
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {isAudioAvailable ? (
-          <FileDisplay 
-            file={file}
-            audioStream={audioStream}
-            handleAudioReset={handleAudioReset}
-          />
+
+        {output ? (
+          <Information />
         ) : (
-          <HomePage 
-            setFile={setFile} 
-            setAudioStream={setAudioStream} 
-          />
+          loading ? (
+            <Transcribing />
+          ) : (
+            isAudioAvailable ? (
+              <FileDisplay 
+                file={file}
+                audioStream={audioStream}
+                handleAudioReset={handleAudioReset}
+              />
+            ) : (
+                <HomePage 
+                  setFile={setFile} 
+                  setAudioStream={setAudioStream} 
+                />
+            )
+          )
         )}
       </section>
-
-      <h1></h1>
       <footer></footer>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
-      <h1 className="font-medium">Free<span className="text-blue-400 bold">Scribe</span></h1>
-      <button className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 specialBtn">
+      <a href="/"><h1 className="font-medium">Free<span className="text-blue-400 bold">Scribe</span></h1></a>
+      <a href="/" className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 text-sm specialBtn">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
-      </button>
+      </a>
     </header>
   )
 }
