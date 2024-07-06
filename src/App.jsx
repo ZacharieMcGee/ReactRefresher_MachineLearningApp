e702447b67d58c41b9ea63096ab110bedfaa0c65
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2024-07-06 16:53:52 -0400

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 import Header from "./components/Header"
 import HomePage from "./components/HomePage"
 import FileDisplay from "./components/FileDisplay"
 import Information from "./components/Information"
 import Transcribing from "./components/Transcribing"
+import { MessageTypes } from "./utils/presets"
 
 function App() {
   const [file, setFile] = useState(null) // audio file
   const [audioStream, setAudioStream] = useState(null) // live recording 
   const [output, setOutput] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [finished, setFinished] = useState(false)
+  const [downloading, setDownloading] = useState(false)
 
   const isAudioAvailable = file || audioStream
 
@@ -18,6 +21,61 @@ function App() {
     setFile(null)
     setAudioStream(null)
   }  
+
+  const worker = useRef(null)
+
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {type: 'module'})
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case 'DOWNLOADING' : setDownloading(true)
+          console.log('DOWNLOADING')
+          break;
+        case 'LOADING' : setDownloading(true)
+          console.log('LOADING')
+          break;
+        case 'RESULT' : setDownloading(true)
+          setOutput(e.data.results)
+          break;
+        case 'INFERENCE_DONE' : setDownloading(true)
+          setFinished(true)
+          console.log('DONE')
+          break;
+      }
+    }
+
+    worker.current.addEventListener('message', onMessageReceived)
+
+    return () => worker.current.removeEventListener('message',onMessageReceived)
+
+  })
+
+  async function readAudioFrom(file) {
+    const samplingRate = 16000
+    const audioContext = new AudioContext({sampleRate: samplingRate})
+    const response = await file.arrayBuffer()
+    const decoded = await audioContext.decodeAudioData(response)
+    const audio = decoded.getChannelData(0)
+    return audio
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) {
+      return
+    }
+
+    let audio = await readAudioFrom(file ? file : audioStream)
+    const model_name = `openai/whisper-tiny.en`
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
+  }
 
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
@@ -35,6 +93,7 @@ function App() {
                 file={file}
                 audioStream={audioStream}
                 handleAudioReset={handleAudioReset}
+                handleFormSubmission={handleFormSubmission}
               />
             ) : (
                 <HomePage 
--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,7 +1,8 @@
 export default function FileDisplay({
   file, 
   audioStream,
-  handleAudioReset
+  handleAudioReset,
+  handleFormSubmission
 }) {
   return (
     <main className="text-center flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center pb-20 w-72 sm:w-96 max-w-full mx-auto">
@@ -14,7 +15,7 @@ export default function FileDisplay({
 
       <div className="flex items-center justify-between gap-4">
         <button onClick={handleAudioReset} className="text-slate-400 hover:text-blue-600 duration-200">Reset</button>
-        <button className="flex items-center gap-2 text-slate-400 specialBtn p-3 font-medium py-2 rounded-lg">
+        <button onClick={handleFormSubmission} className="flex items-center gap-2 text-slate-400 specialBtn p-3 font-medium py-2 rounded-lg">
           <p>Transcribe</p>
           <i className="fa-solid fa-pen-nib"></i>
         </button>
